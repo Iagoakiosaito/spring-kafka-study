089b98f692c08e209d50fa5cb59f3351eea07be0
Iago_saito <iagoosaito@gmail.com>
2023-11-16 09:08:53 -0400

    featÃ: adição de múltiplos consumidores para consumo de múltiplas partições de um tópico

--- consumer/src/main/java/br/com/gubee/mensageria/consumer/ConsumerService.java
+++ consumer/src/main/java/br/com/gubee/mensageria/consumer/ConsumerService.java
@@ -1,9 +1,8 @@
 package br.com.gubee.mensageria.consumer;
-
-import br.com.gubee.mensageria.model.Order;
 import br.com.gubee.mensageria.model.OrderEvent;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -16,14 +15,36 @@ public class ConsumerService {
 
     Map<String, Set<Object>> consumedRecords = new ConcurrentHashMap<>();
 
-    @KafkaListener(topics = "order_created", groupId = "group_id-3", containerFactory = "orderKafkaListenerContainerFactory", concurrency = "3")
-    public void consumeOrder(ConsumerRecord<?, ?> consumerRecord) {
+    @KafkaListener(topicPartitions = @TopicPartition(topic = "order_created", partitions = {"0"}), groupId = "group_id", containerFactory = "orderKafkaListenerContainerFactory")
+    public void consumeOrderPartition0(ConsumerRecord<?, ?> consumerRecord) {
         trackConsumedPartitions("consumer-1", consumerRecord.partition());
+        System.out.println("Consumer-0");
         System.out.println(consumerRecord.partition());
         System.out.println("Thread ID: " + Thread.currentThread().getId());
+        System.out.println("-----------------------");
+
     }
 
-    @KafkaListener(topics = "order_status", groupId = "group_id-3", containerFactory = "orderEventKafkaListenerContainerFactory", concurrency = "3")
+    @KafkaListener(topicPartitions = @TopicPartition(topic = "order_created", partitions = {"1"}), groupId = "group_id", containerFactory = "orderKafkaListenerContainerFactory")
+    public void consumeOrderPartition1(ConsumerRecord<?, ?> consumerRecord) {
+        trackConsumedPartitions("consumer-2", consumerRecord.partition());
+        System.out.println("Consumer-1");
+        System.out.println(consumerRecord.partition());
+        System.out.println("Thread ID: " + Thread.currentThread().getId());
+        System.out.println("-----------------------");
+
+    }
+
+    @KafkaListener(topics = "order_created", groupId = "group_id", containerFactory = "orderKafkaListenerContainerFactory")
+    public void consumeOrderAllPartitions(ConsumerRecord<?, ?> consumerRecord) {
+        trackConsumedPartitions("consumer-3", consumerRecord.partition());
+        System.out.println("Consumer-3");
+        System.out.println(consumerRecord.partition());
+        System.out.println("Thread ID: " + Thread.currentThread().getId());
+        System.out.println("-----------------------");
+    }
+
+    @KafkaListener(topics = "order_status", groupId = "group_id", containerFactory = "orderEventKafkaListenerContainerFactory")
     public void consumeOrderEvent(OrderEvent event) {
         System.out.println(event);
         System.out.println("Thread ID: " + Thread.currentThread().getId());
